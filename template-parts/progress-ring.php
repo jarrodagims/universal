050994f793724cc44645d8b6639fc673322b8f10
Jarrod Medrano <jmedran@gmail.com>
2018-10-25 15:13:28 -0600

    coding the circle

---
+++ template-parts/progress-ring.php
@@ -0,0 +1,31 @@
+<div class="circle">80<span class="perc">%</span>
+<!--    <svg-->
+<!--        class="progress-ring"-->
+<!--        width="160"-->
+<!--        height="160">-->
+<!--        <circle-->
+<!--            class="progress-ring-circle"-->
+<!--            stroke="white"-->
+<!--            stroke-width="4"-->
+<!--            fill="transparent"-->
+<!--            r="72"-->
+<!--            cx="80"-->
+<!--            cy="80"></circle>-->
+<!--    </svg>-->
+<!--    <input-->
+<!--        value="35"-->
+<!--        type="number"-->
+<!--        step="5"-->
+<!--        min="0"-->
+<!--        max="100"-->
+<!--        placeholder="progress"-->
+<!--    >-->
+
+    <div id="circle-line">
+        <svg width="160px" height="160px" viewBox="0 0 38 38" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <circle id="circle-line-path" stroke-width="4" cx="19" cy="19" r="17"></circle>
+            </g>
+        </svg>
+    </div>
+</div>
