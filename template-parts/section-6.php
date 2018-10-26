c504f2eecfbe6978efd7f2b77a08241472433770
Jarrod Medrano <jmedran@gmail.com>
2018-10-26 14:01:28 -0600

    styling module 6

--- template-parts/section-6.php
+++ template-parts/section-6.php
@@ -3,13 +3,22 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col col-md-4">
                 <h2>
                     Work With The Best
                 </h2>
                 <p>
                     First Google Partner Agency in El Paso
                 </p>
+            </div>
+            <div class="col col-md-8">
+                <div class="badges flex-row">
+                    <div class="badge top-social"></div>
+                    <div class="badge top-design"></div>
+                    <div class="badge top-seo"></div>
+                    <div class="badge digital"></div>
+                    <div class="badge top-ppc"></div>
+                </div>
             </div>
         </div>
     </div>
