696df55d340821a0887f2e2c8e1c52e8b7601be0
Jarrod Medrano <jmedran@gmail.com>
2018-10-26 16:09:05 -0600

    styling form

--- template-parts/section-6.php
+++ template-parts/section-6.php
@@ -2,13 +2,13 @@
 
 
     <div class="container">
-        <div class="row">
+        <div class="row flex-row  top-contact" >
             <div class="col col-md-5 flex-col">
                 <div class="swish"><h2>
                         Work With <strong>The <span class="brand">Best</span></strong>
                 </h2>
                 <p>
-                    First <span class="google-logo">Google Partner</span> Agency in El Paso
+                    First <span class="google-logo">Google Partner</span> <strong>Partner Agency</strong> in El Paso
                 </p></div>
             </div>
             <div class="col col-md-7">
@@ -19,6 +19,16 @@
                     <div class="upcity-badge digital"></div>
                     <div class="upcity-badge top-ppc"></div>
                 </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container contact-bottom">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Contact Us</h2>
+                <h3>I'd Like to Learn More</h3>
+                <?php echo do_shortcode('[contact-form-7 id="41" title="Contact Form"]'); ?>
             </div>
         </div>
     </div>
