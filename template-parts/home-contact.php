55cf9bc5069f64f048288acad0cbe66aa0af8c34
Jarrod Medrano <jmedran@gmail.com>
2018-10-30 15:37:08 -0600

    styling the mobile

--- template-parts/home-contact.php
+++ template-parts/home-contact.php
@@ -11,14 +11,20 @@
                     First <span class="google-logo">Google Partner</span> <strong>Partner Agency</strong> in El Paso
                 </p></div>
             </div>
-            <div class="col col-md-7">
-                <div class="badges">
-                    <div class="upcity-badge top-social"></div>
-                    <div class="upcity-badge top-design"></div>
-                    <div class="upcity-badge top-seo"></div>
-                    <div class="upcity-badge digital"></div>
-                    <div class="upcity-badge top-ppc"></div>
-                </div>
+            <div class="col flex-col col-md-7">
+                <picture>
+                    <source media="(min-width: 992px)" srcset="<?= IMGURL ?>badges-large.png">
+                    <img src="<?= IMGURL ?>badges.png" alt="Badges" />
+                </picture>
+
+
+<!--                <div class="badges">-->
+<!--                    <div class="upcity-badge top-social"></div>-->
+<!--                    <div class="upcity-badge top-design"></div>-->
+<!--                    <div class="upcity-badge top-seo"></div>-->
+<!--                    <div class="upcity-badge digital"></div>-->
+<!--                    <div class="upcity-badge top-ppc"></div>-->
+<!--                </div>-->
             </div>
         </div>
     </div>
@@ -28,7 +34,7 @@
             <div class="col-md-12">
                 <h2>Contact Us</h2>
                 <h3>I'd Like to Learn More</h3>
-                <?php echo do_shortcode('[contact-form-7 id="9" title="Contact form 1"]'); ?>
+                <?php echo do_shortcode('[contact-form-7 id="41" title="Contact"]'); ?>
             </div>
         </div>
     </div>
