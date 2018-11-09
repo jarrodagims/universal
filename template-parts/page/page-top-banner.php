09c9a3cfb951b7955bebdc258adc327053ad6042
Jarrod Medrano <jmedran@gmail.com>
2018-11-09 15:58:24 -0700

    google partner banner changes

--- template-parts/page/page-top-banner.php
+++ template-parts/page/page-top-banner.php
@@ -7,19 +7,33 @@
     if (has_post_thumbnail( $post->ID ) ):
     $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
     if($backgroundImg) : ?>
-        <div class="top-banner" style="background-image: url('<?php echo $backgroundImg[0]; ?>')">
+        <div class="top-banner <?php if(is_page('google-partner')) : ?>google-banner<?php endif; ?>" style="background-image: url('<?php echo $backgroundImg[0]; ?>')">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
            <?php echo $title; ?>
 
             <?php if(!is_page(293)) : ?>
+
             <a href="<?= SITEURL ?>/contact-us/"><button class="btn btn-success">Contact Us</button></a>
+
             <?php endif; ?>
+                    </div>
+                </div>
+            </div>
         </div>
 <?php endif;
 else:
     ?>
         <div class="top-banner" style="background-image: url('<?= IMGURL ?>bg-building-top.jpg');"  alt="<?php echo get_bloginfo( 'name' ); ?>">
-            <?php echo $title; ?>
-            <a href="<?= SITEURL ?>/contact-us/"><button class="btn btn-success">Contact Us</button></a>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <?php echo $title; ?>
+                        <a href="<?= SITEURL ?>/contact-us/"><button class="btn btn-success">Contact Us</button></a>
+                    </div>
+                </div>
+            </div>
         </div>
 <?php
 endif;
