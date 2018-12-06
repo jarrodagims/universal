762c90e474b2006dd17b036728ee437e594180cf
Jarrod Medrano <jmedran@gmail.com>
2018-12-06 09:01:09 -0700

    slideshow edit

--- header.php
+++ header.php
@@ -56,23 +56,6 @@
     <?= get_option('sherpa_schema') ?>
 
     <?php endif; ?>
-    <link rel="apple-touch-icon" sizes="57x57" href="<?= SITEURL ?>/icons/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?= SITEURL ?>/icons/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?= SITEURL ?>/icons/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?= SITEURL ?>/icons/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?= SITEURL ?>/icons/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?= SITEURL ?>/icons/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?= SITEURL ?>/icons/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?= SITEURL ?>/icons/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= SITEURL ?>/icons/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="<?= SITEURL ?>/icons/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= SITEURL ?>/icons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?= SITEURL ?>/icons/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= SITEURL ?>/icons/favicon-16x16.png">
-    <link rel="manifest" href="<?= SITEURL ?>/icons/manifest.json">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="<?= SITEURL ?>/icons/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
 	
 	<script type="application/ld+json">
 	{
--- template-parts/home/home-badges.php
+++ template-parts/home/home-badges.php
@@ -6,7 +6,7 @@
                         Work With <strong>The <span class="brand">Best</span></strong>
                     </h2>
                     <p>
-                        First <img src="<?= IMGURL ?>google.svg" alt="Google" class="google-logo" /> <strong>Partner Agency</strong> in El Paso
+                        A <img src="<?= IMGURL ?>google.svg" alt="Google" class="google-logo" /> <strong>Partner Agency</strong>
                     </p></div></div>
         </div>
         <div class="col flex-col col-lg-7">
@@ -21,8 +21,6 @@
                          class="upcity-badge top-digital"/></li>
                 <li class="waypoints outer"><img src="<?= IMGURL ?>badge-ppc.png" alt="Badge PPC" class="upcity-badge top-ppc"/></li>
             </ul>
-
-
         </div>
     </div>
 </div>
