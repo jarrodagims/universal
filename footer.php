7cead7345a495b1d59d821c1d62cd470eb98e75c
Jarrod Medrano <jmedran@gmail.com>
2018-12-10 10:37:21 -0700

    resetting

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <div id="map-section">
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3393.4467991876736!2d-106.29499158484415!3d31.731003281300335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86e7436466f74de9%3A0x89cd8fa03f0ca35!2sAGI+Marketing!5e0!3m2!1sen!2sus!4v1539116531397"
+
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13573.166109789012!2d-106.30427626683618!3d31.7352427007831!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86e7437ed7c9de57%3A0x8cdcad40407564ef!2squality+iron+maid!5e0!3m2!1sen!2sus!4v1544221567195"
             width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
 <section id="footer">
@@ -11,6 +12,7 @@
                             alt="<?php echo get_bloginfo(
                                 'description'
                             ); ?>"/></a>
+                <p>If you’re looking for ironing clothes service in El Paso, Quality Iron Maid can help.</p>
             </div>
             <div class="col phone-footer">
                 <a href="tel:9152095381">915.209.5381</a>
@@ -30,19 +32,13 @@
                 </div>
             </div>
             <div class="col social-footer">
-<!--                --><?php //$sm = new SocialMedia(array('facebook', 'twitter', 'linkedin', 'youtube'));
-//                $sm->setSize('md');
-//                $sm->showNetworkButtons();
-//                $sm->setColorType('singleColor');
-//                ?>
-
-                <ul class="list-inline social-media lg">
-                    <li><a href="https://www.facebook.com/agimarketing" class="facebook-button social-media-button lg square lighten external" target="_blank"><i class="fab fa-facebook-f fa-fw fa-lg"><span>Facebook</span></i></a></li>
-                    <li><a href="https://twitter.com/agi_marketing" class="twitter-button social-media-button lg square lighten external" target="_blank"><i class="fab fa-twitter fa-fw fa-lg"><span>Twitter</span></i></a></li>
-                    <li><a href="https://www.linkedin.com/company/agi-marketing" class="linkedin-button social-media-button lg square lighten external" target="_blank"><i class="fab fa-linkedin-in fa-fw fa-lg"><span>LinkedIn</span></i></a></li>
-                    <li><a href="https://www.youtube.com/channel/UCG6VrBxIYi2M5lmxkJWTLJw" class="youtube-button social-media-button lg square lighten external" target="_blank"><i class="fab fa-youtube fa-fw fa-lg"><span>YouTube</span></i></a></li>
-                </ul>
-                <p class="small">Privacy Policy | Site Copyright &copy; <?php echo date('Y'); ?> <img class="logo-sm" src="<?= IMGURL ?>agi-cool.svg" alt="AGI Marketing" /></p>
+<?php
+$sm = new SocialMedia(array('facebook', 'twitter'));
+$sm->setSize('md');
+$sm->showNetworkButtons();
+$sm->setColorType('singleColor');
+?>
+                <p class="small">Privacy Policy | Site Copyright &copy; <?php echo date('Y'); ?></p>
             </div>
         </div>
     </div>
