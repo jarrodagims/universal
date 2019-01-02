a2db99a91af70e86f8c460455e5bce64656ed344
Jarrod Medrano <jmedran@gmail.com>
2019-01-02 14:30:40 -0700

    initial commit

--- footer.php
+++ footer.php
@@ -1,8 +1,3 @@
-<div id="map-section">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27142.53913410327!2d-106.31983605273021!3d31.748184996926778!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86e744943a05e675%3A0x2141452b410c938!2sCrown+and+Eagle+Bar+%26+Grill!5e0!3m2!1sen!2sus!4v1544733236184"
-        width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
-
-</div>
 <section id="footer">
     <div class="container">
         <div class="row align-items-center">
@@ -17,31 +12,24 @@
                             ); ?>" /></a>
                     </div>
                     <div class="col">
-                        <span class="title">Contact</span>
-                        <p>Hit us up on the east side of town! El Paso’s best patio bar.
-                            Crown and Eagle bar and grill at our convenient location.</p>
+                        <p>Please feel free to contact us.
+                            You can either call our toll-free number or check below to see if there is an office close
+                            to your area.
+
                     </div>
                     <div class="col phone-footer">
                         <div>
-                            <span class="title">&nbsp;</span>
-                            <p>1731 Lee Trevino<br />
-                                El Paso, Texas 79936<br />
-                                <strong>Call Us Today!</strong><br />
-                                <a href="tel:<?php if (!empty(get_option('sherpa_telephone_number'))) : echo get_option('sherpa_telephone_number'); endif; ?>">
-                                    <?php printPhone(); ?></a>
-                            </p>
+                            <span class="title">Contact</span>
+                            <p>Hail Repair: Call Carlos: (903) 285-6438
+                                Non-Hail Dent Repair: Call Derek: (915) 256-4050
+                                Authority Automotive: Call Rick Stumvoll: (214) 766-5127
+                                Master Shine: Call Carlos Ropez: (469) 951-0745</p>
                         </div>
                     </div>
 
                 </div>
             </div>
-            <div class="col col-md-2 address-footer">
-                <div><span class="title">OFFICE HOURS</span>
-                    <p>Monday - Sunday:<br />
-                        11:00am - 2:00am</p>
-                    <img src="<?=IMGURL?>uber-eats-btn.svg" alt="Uber Eats" height="32" />
-                </div>
-            </div>
+
             <!-- <div class="d-md-none d-xs-block d-sm-block">
                 <?php
 $sm = new SocialMedia(array('facebook', 'twitter'));
@@ -84,7 +72,7 @@ $sm->setColorType('singleColor');
                 </div>
                 <p class="small">
                     &copy;
-                    <?php echo date('Y'); ?> Crown And Eagle Bar And Grill | All Rights Reserved
+                    <?php echo date('Y'); ?> C&D Dent Guys | All Rights Reserved
                 </p>
 
             </div>
--- header.php
+++ header.php
@@ -28,53 +28,61 @@
 </head>
 
 <body <?php body_class(); ?>>
-
-    <!--[if lt IE 7]>
+    <div class="landing-bg">
+        <!--[if lt IE 7]>
     <p class="browsehappy">You are using an <strong>outdated</strong> browser.</p>
     <p class="browsehappy">Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.
     </p>
     <![endif]-->
-    <div class="top-header <?php if(!is_front_page()) : ?>internal<?php endif; ?>">
-        <div class="container">
-            <div class="v-align">
-                <div class="col-xs-12 navbar-button text-center">
-                    <div class="navbar-left">
-                        <ul class="nav navbar-nav navbar-right">
-                            <li class="menu-item directions"><a href="<?php echo get_site_url() ?>/contact-us/"><span>6455
-                                        Hiller Suite 10, El Paso, Texas 79925</span></a></a></li>
-                            <li class="menu-item contact"><a href="tel:<?php if (!empty(get_option('sherpa_telephone_number'))) : echo get_option('sherpa_telephone_number'); endif; ?>"><span>Call
-                                        Us Today!
-                                        <?php printPhone(); ?></span></a></li>
-                        </ul>
-                        <a href="/"><img src="<?=IMGURL?>uber-eats-btn.svg" alt="Uber Eats" height="22" /></a>
+        <div class="top-header <?php if(!is_front_page()) : ?>internal<?php endif; ?>">
+            <div class="container">
+                <div class="v-align">
+                    <div class="col-xs-12 navbar-button text-center">
+                        <a class="navbar-brand" href='<?php echo get_site_url(); ?>'><img src="<?= IMGURL; ?>logo.png"
+                                alt="<?php echo get_bloginfo(
+                                'description'
+                            ); ?>" /></a>
+
+                        <?php echo responsive_bs_menu('primary', 'left', SITENAME); ?>
+
+                        <div class="navbar-right">
+
+                            <ul class="nav navbar-nav navbar-right">
+                                <li>EL PASO, TX 915-642-6911</li>
+                                <li class="menu-item directions"><a href="<?php echo get_site_url() ?>/contact-us/"><span>6455
+                                            Hiller Suite 10, El Paso, Texas 79925</span></a></a></li>
+                                <li class="menu-item contact"><a href="tel:<?php if (!empty(get_option('sherpa_telephone_number'))) : echo get_option('sherpa_telephone_number'); endif; ?>"><span>Call
+                                            Us Today!
+                                            <?php printPhone(); ?></span></a></li>
+                            </ul>
+
+                        </div>
 
                     </div>
-                    <?php echo responsive_bs_menu('primary', 'left', SITENAME); ?>
                 </div>
             </div>
         </div>
-    </div>
 
-    <?php if(!is_front_page()) : ?>
-    <div class="banner-header">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <a href="<?=SITEURL?>" class="navbar-brand"><img src="<?=IMGURL?>logo-lg.png" class="navbar-brand"
-                            alt="<?php bloginfo( 'name' ); ?>" /></a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <section class="home-module-2">
-        <section class="icon-row">
+        <?php if(!is_front_page()) : ?>
+        <div class="banner-header">
             <div class="container">
                 <div class="row">
                     <div class="col">
-                        <?php echo get_template_part('template-parts/icon', 'row'); ?>
+                        <a href="<?=SITEURL?>" class="navbar-brand"><img src="<?=IMGURL?>logo-lg.png" class="navbar-brand"
+                                alt="<?php bloginfo( 'name' ); ?>" /></a>
                     </div>
                 </div>
             </div>
+        </div>
+        <section class="home-module-2">
+            <section class="icon-row">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <?php echo get_template_part('template-parts/icon', 'row'); ?>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </section>
-    </section>
-    <?php endif; ?>
+        <?php endif; ?>
