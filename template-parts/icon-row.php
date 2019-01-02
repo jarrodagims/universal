ab7e1b92fe301289da31aaa0a479f955774b9fa7
Jarrod Medrano <jmedran@gmail.com>
2019-01-02 15:55:49 -0700

    styling icons

--- template-parts/icon-row.php
+++ template-parts/icon-row.php
@@ -1,50 +1,51 @@
 <ul class="flex-row hidden-xs hidden-sm">
     <li class="flex-col">
         <a href="">
-            <?php echo get_template_part('template-parts/icons/icon','1') ?>
-            $0 <span>(Out Of Pocket)</span>
-            hail repair in
-            Odessa & Midland</a>
+            <?php echo get_template_part('template-parts/icons/icon','1') ?></a>
+
+        $0 <span>(Out Of Pocket)</span>
+        hail repair in
+        Odessa & Midland
     </li>
     <li class="flex-col">
         <a href="">
-            <?php echo get_template_part('template-parts/icons/icon','2') ?>
-            $200
-            Gift Card</a>
+            <?php echo get_template_part('template-parts/icons/icon','2') ?></a>
+        $200
+        Gift Card
     </li>
     <li class="flex-col">
         <a href="">
-            <?php echo get_template_part('template-parts/icons/icon','3') ?>
-            We Pay Your
-            Deductible
-            $0 <span>(Out Of Pocket)</span></a>
+            <?php echo get_template_part('template-parts/icons/icon','3') ?></a>
+        We Pay Your
+        Deductible
+        $0 <span>(Out Of Pocket)</span>
     </li>
 
     <li class="flex-col">
         <a href="">
-            <?php echo get_template_part('template-parts/icons/icon','4') ?>
-            Free Pick Up
-            & Delivery</a>
+            <?php echo get_template_part('template-parts/icons/icon','4') ?></a>
+        Free Pick Up
+        & Delivery
     </li>
 
     <li class="flex-col">
         <a href="">
-            <?php echo get_template_part('template-parts/icons/icon','5') ?>
-            Free
-            Rental Car</a>
+            <?php echo get_template_part('template-parts/icons/icon','5') ?></a>
+        Free
+        Rental Car
     </li>
 
     <li class="flex-col">
         <a href="">
-            <?php echo get_template_part('template-parts/icons/icon','6') ?>
-            We Work With
-            All Insurance</a>
+            <?php echo get_template_part('template-parts/icons/icon','6') ?></a>
+        We Work With
+        All Insurance
     </li>
 
     <li class="flex-col">
         <a href="">
-            <?php echo get_template_part('template-parts/icons/icon','7') ?>
-            $100 For
-            Referrals</a>
+            <?php echo get_template_part('template-parts/icons/icon','7') ?></a>
+        $100 For
+        Referrals
     </li>
 </ul>
