1c097ccb820f2e3889979734e6530e40ad4cb5c0
Jarrod Medrano <jmedran@gmail.com>
2019-01-29 09:01:17 -0700

    styling footer

--- page-home.php
+++ page-home.php
@@ -8,7 +8,7 @@
         <div class="jumbo-content">
             <h2>Home Builder</h2>
             <h1>BUILDING HOMES THAT
-                LOOK ALMOST AS GOOD AS YOU!</h1>
+                <span class="br-md">LOOK ALMOST AS GOOD AS YOU!</span></h1>
 
             <a href="<?=SITEURL?>/contact-us/" class="contact-us">
                 <button class="btn btn-primary">
@@ -47,4 +47,248 @@
         </div>
     </div>
 </section>
+
+<section class="ribbon">
+
+</section>
+
+<section class="home-module-3">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="row">
+                    <div class="col">
+                        <h2>FLOORPLANS</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+
+
+                        <h3>POPLIN</h3>
+                    </div>
+                    <div class="col">
+                        <ul>
+                            <li>FLOORPLAN</li>
+                            <li>LIVING AREA</li>
+                            <li>BEDROOMS</li>
+                            <li>BATHEOOMS</li>
+                            <li>PRICE</li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        SINGLE STORY<br />
+                        1750 SQ. FT<br />
+                        4<br />
+                        2<br />
+                        <span class="brand">$280,000</span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <a href="<?=SITEURL?>/contact-us/" class="contact-us">
+                            <button class="btn btn-primary">
+                                SCHEDULE A TOUR
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <img src="<?=IMGURL?>3d.png" alt="3d View">
+            </div>
+        </div>
+    </div>
+    <ul class="slide-nav">
+        <li>POPLIN</li>
+        <li>PAISLEY</li>
+        <li>CAMBRIC</li>
+        <li>VELOUR</li>
+        <li>TRELLIS</li>
+    </ul>
+</section>
+
+<section class="home-module-4">
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h2>THE ANSWER IS SIMPLE.
+                    WE UNDERSTAND THE NEEDS OF EL PASO RESIDENTS. </h2>
+
+                <p>
+                    Just like ordering a dish that contains all the right flavors, we construct homes with all the right
+                    ingredients and the finished product is one you’ll certainly enjoy. Like our saying goes, we’re the
+                    cheese to your enchiladas and we say that for a reason!
+                </p>
+
+                <p>Our decades of experience as a home builder in the Sun City has given us the ability to test out
+                    different ingredients, play with design, then share them with the community. We have a keen
+                    understanding of comfort, luxury, and quality. You’ll find that our homes incorporate these traits
+                    into the aesthetics both indoors and outdoors. From the moment you arrive at your front driveway to
+                    walking into the hallway that leads to the living space — we bring to life an opulence that can be
+                    felt throughout the entire home. Just like every bite you take should taste delicious, we believe in
+                    providing that same goodness.</p>
+
+            </div>
+            <div class="col">
+
+
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section class="ribbon-2">
+    <div class="container">
+        <div class="row">
+            <div class="col d-flex justify-content-md-start justify-content-center align-items-center">
+                CONTACT US TODAY! <a href="<?=SITEURL?>/contact-us/" class="contact-us">
+                    <button class="btn btn-secondary">
+                        CONTACT US
+                    </button>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="home-module-5">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+            </div>
+            <div class="col">
+                <h2>YOU KNOW WHAT MAKES US GOOD…
+                    <span class="br-md">HERE’S WHAT MAKES US THE BEST</span>
+
+                </h2>
+
+                <p>
+                    Our secret family recipe consists of building high-quality homes at affordable prices. Once you add
+                    in excellent customer service, you have a recipe for success! As a home builder who places their
+                    future homeowners at the forefront of everything we do, we have plenty of additional upgrades to
+                    choose from, we guarantee you will find exactly what you’re looking for, plus more! </p>
+
+                <a href="<?=SITEURL?>/contact-us/" class="contact-us">
+                    <button class="btn btn-primary">
+                        LEARN MORE
+                    </button>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="home-module-6">
+    <div class="container">
+
+        <div class="row">
+            <div class="col">
+
+                <h2>HERE’S WHAT YOU GET WHEN YOU CHOOSE ZIA HOMES
+                    <span class="br-md">AS YOUR HOME BUILDER:</span></h2>
+
+                <div class="row">
+
+                    <div class="col">
+                        <div class="mini-card"><img src="<?=IMGURL?>landscaping.jpg" alt="">
+                            <h3>BEAUTIFUL
+                                LANDSCAPING</h3>
+                            <p>At Zia Homes, we believe that a beautiful home begins from the outside in. Whether you
+                                are
+                                interested in the addition of grass, trees or rocks, we can customize your yard
+                                according to
+                                your liking. Curb appeal will be heightened to the max! </p>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="mini-card"><img src="<?=IMGURL?>partners.jpg" alt="">
+                            <h3>
+                                A PARTNER WHO
+                                COMMUNICATES</h3>
+                            <p>There’ll be no shortage of emails, phone calls, or text
+                                message to go around! Because when you choose to work with us, we take that relationship
+                                seriously. We’re always attentive to your needs and do our best to provide you with
+                                answers
+                                quickly and thoughtfully. </p>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="mini-card"><img src="<?=IMGURL?>buyers.jpg" alt="">
+                            <h3>
+                                A COMPREHENSIVE
+                                BUYER’S WARRANTY
+                            </h3>
+                            <p>Feel confident knowing the home of your dreams is under warranty. Zia Homes offers an
+                                all-inclusive warranty that includes a two-year systems warranty and a 10 year extended
+                                warranty. So even if there are hiccups along the way, they will be gone in no time!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section id="footer" class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col contact-form">
+                <h2>
+                    Ready To Meet Your Future Home?
+                    It’s Waiting To Meet You Too!
+                </h2>
+
+                <p>
+                    We know the excitement can be hard to contain and at the same time, it can feel extremely
+                    overwhelming. But not to worry, as the saying goes, an action is the solution to fear. At Zia Homes,
+                    we’ll help guide you in finding the perfect home. Our team will gladly answer your initial questions
+                    and guide you down the path of becoming a happy new homeowner. <a href="tel:
+9155918153">Give us a
+                        call today</a>!</p>
+
+
+                <div id="contact-form">
+                    <?php if ($_SERVER['REMOTE_ADDR'] == '127.0.0.1') {
+                echo do_shortcode('[contact-form-7 id="62" title="Contact Form"]');
+            } else {
+                echo do_shortcode('[contact-form-7 id="62" title="Contact Form"]');
+            }
+            ?>
+                </div>
+            </div>
+
+            <div class="col contact-box">
+                <div>
+                    <h3>
+                        CONTACT</h3>
+
+                    <p>2244 Trawood Dr # 207, El Paso, Tx 79935</p>
+
+                    <h3>OFFICE HOURS</h3>
+
+                    <p>Monday – Friday: 9 AM – 5 PM<br />
+                        Saturday: By Appointment</p>
+
+                    <?php
+                $sm = new SocialMedia(array('facebook', 'twitter'));
+                $sm->setSize('sm');
+                $sm->showNetworkButtons();
+                $sm->setColorType('singleColor');
+                ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="sub-footer">
+    &copy; <?php echo date('Y'); ?> Zia Homes | All Rights Reserved
+</div>
 <?php get_footer(); ?>
