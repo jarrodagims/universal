2ff9ab0237e385b0763c601b6fed42721fc22bfc
Jarrod Medrano <jmedran@gmail.com>
2019-02-01 16:28:06 -0700

    styling about page

--- page-floorplans.php
+++ page-floorplans.php
@@ -20,6 +20,8 @@
                             <li>LOWER VALLEY</li>
                         </ul>
 
+
+
                         <h2 class="text-center">WESTSIDE EL PASO</h2>
 
 
@@ -90,13 +92,13 @@
                                 </div>
 
                                 <div class="col">
-                                    <div>
+                                    <div class="coming-soon">
 
                                         <h2>CASHMERE
                                         </h2>
 
-                                        <p>
-                                            2672 SQ. FT</p>
+                                        <h3>
+                                            2672 SQ. FT</h3>
 
                                         <p>
                                             COMING SOON</p>
@@ -129,13 +131,13 @@
                                 </div>
 
                                 <div class="col">
-                                    <div>
+                                    <div class="coming-soon">
 
                                         <h2>CASHMERE
                                         </h2>
 
-                                        <p>
-                                            2672 SQ. FT</p>
+                                        <h3>
+                                            2672 SQ. FT</h3>
 
                                         <p>
                                             COMING SOON</p>
@@ -144,6 +146,52 @@
                             </div>
                         </div>
 
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col">
+                                <h1>New Houses for Sale in the El Paso Area</h1>
+
+                                <p>
+                                    At Zia Homes, we build a wide spectrum of new homes. Whether you’re looking for a
+                                    completely move-in ready home or wanting to build from the ground up, we can help
+                                    you find exactly what you’re looking for, and maybe a little more.
+                                </p>
+                            </div>
+                            <div class="col">
+                                <h2>
+                                    About Our New Homes for Sale
+                                </h2>
+                                <p>Our current homes start at 1,335 square feet and can go up to 2,705. Floor plans
+                                    generally feature 3 bedrooms, 2 baths, a kitchen with granite countertops, and a
+                                    master bedroom with a spacious walk-in closet. Our houses for sale provide the
+                                    efficiency of new appliances when you choose to go with our valued partner. Other
+                                    features of our homes include a large kitchen with plenty of cabinet space.</p>
+
+                                <p>In terms of price, our houses for sale start in the $150’s on the east side and in
+                                    the $160’s on the west side. If you’re interested in customizations, please schedule
+                                    a consultation or come visit our office so we can discuss your options.
+                                </p>
+                            </div>
+
+                            <div class="col">
+                                <h2>
+                                    Your New Home Awaits You
+                                </h2>
+
+                                <p>
+                                    If there’s one thing that Zia Homes does best, it’s that it combines functionality
+                                    with design and gives you the freedom to fill a home with your most-prized
+                                    possessions and begin building new memories. As we build new homes, we always keep
+                                    you—the future owner—in mind. Be sure to check out our list of new homes for sale on
+                                    a daily basis as we consistently update our inventory after one home sells and a new
+                                    one is built.
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
