ed8477f1c7474aee1915612ab636d5e68bf40161
Jarrod Medrano <jmedran@gmail.com>
2019-05-09 16:53:11 -0600

    before leaving

--- header.php
+++ header.php
@@ -113,12 +113,23 @@
                                             else: echo 'Blog';
                                             endif; ?></div>
                                                 <img src="<?=IMGURL?>find-us-on.svg" alt="Find Us On">
-                                                <?php
-                                            $sm = new SocialMedia(array('facebook', 'twitter'));
-                                            $sm->setSize('sm');
-                                            $sm->showNetworkButtons();
-                                            $sm->setColorType('singleColor');
-                                            ?>
+                                                <ul class="list-inline social-media sm">
+                                                    <li><a href="https://www.facebook.com/UniversalGraphicsInc/"
+                                                            class="facebook-button social-media-button sm square lighten external"
+                                                            target="_blank"><i
+                                                                class="fab fa-facebook-f fa-fw "><span>Facebook</span></i></a>
+                                                    </li>
+                                                    <li><a href="https://twitter.com/gil_the_printer"
+                                                            class="twitter-button social-media-button sm square lighten external"
+                                                            target="_blank"><i
+                                                                class="fab fa-twitter fa-fw "><span>Twitter</span></i></a>
+                                                    </li>
+                                                    <li><a href="https://www.instagram.com/universalgraphicsinc/"
+                                                            class="instagram-button social-media-button sm square lighten external"
+                                                            target="_blank"><i
+                                                                class="fab fa-instagram fa-fw "><span>Instagram</span></i></a>
+                                                    </li>
+                                                </ul>
                                             </div>
                                         </div>
                                     </div>
@@ -169,12 +180,23 @@
                                             else: echo 'Blog';
                                             endif; ?></div>
                                                 <img src="<?=IMGURL?>find-us-on.svg" alt="Find Us On">
-                                                <?php
-                                            $sm = new SocialMedia(array('facebook', 'twitter'));
-                                            $sm->setSize('sm');
-                                            $sm->showNetworkButtons();
-                                            $sm->setColorType('singleColor');
-                                            ?>
+                                                <ul class="list-inline social-media sm">
+                                                    <li><a href="https://www.facebook.com/UniversalGraphicsInc/"
+                                                            class="facebook-button social-media-button sm square lighten external"
+                                                            target="_blank"><i
+                                                                class="fab fa-facebook-f fa-fw "><span>Facebook</span></i></a>
+                                                    </li>
+                                                    <li><a href="https://twitter.com/gil_the_printer"
+                                                            class="twitter-button social-media-button sm square lighten external"
+                                                            target="_blank"><i
+                                                                class="fab fa-twitter fa-fw "><span>Twitter</span></i></a>
+                                                    </li>
+                                                    <li><a href="https://www.instagram.com/universalgraphicsinc/"
+                                                            class="instagram-button social-media-button sm square lighten external"
+                                                            target="_blank"><i
+                                                                class="fab fa-instagram fa-fw "><span>Instagram</span></i></a>
+                                                    </li>
+                                                </ul>
                                             </div>
                                         </div>
                                     </div>
