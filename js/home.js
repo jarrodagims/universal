a77599a5de444af4102f5d9951bbeb51f052947e
Jarrod Medrano <jmedran@gmail.com>
2019-05-15 16:56:57 -0600

    updates

--- js/home.js
+++ js/home.js
@@ -12,22 +12,6 @@
       );
     });
 
-    //disable logs
-    $.fn.cycle.log = $.noop;
-
-    $(".waypoint").each(function() {
-      var self = $(this);
-
-      $(this).waypoint({
-        handler: function() {
-          self.addClass("active");
-          //disable after activating once
-          this.destroy();
-        },
-        offset: "50%"
-      });
-    });
-
     //stick nav
     function stickyExample() {
       var $stickyElement = $(".sticky");
@@ -40,44 +24,5 @@
     }
 
     stickyExample();
-
-    (function() {
-      var deskFunctions = function() {
-        //make dropdown hoverable
-        //$(".dropdown-toggle").attr("data-toggle", "hover");
-        // $(".top-header").prependTo(".jumbo-container");
-      };
-
-      var mobileFunctions = function() {
-        //make dropdown clickable
-        //$(".dropdown-toggle").attr("data-toggle", "dropdown");
-        // $(".top-header").prependTo(".sidebar-container");
-      };
-
-      //Only do slideshow on small screens
-      var timeout;
-      window.addEventListener(
-        "resize",
-        function(event) {
-          if (timeout) {
-            window.cancelAnimationFrame(timeout);
-          }
-          timeout = window.requestAnimationFrame(function() {
-            resize();
-          });
-        },
-        false
-      );
-
-      function resize() {
-        if ($(window).width() >= 992) {
-          deskFunctions();
-        } else if ($(window).width() < 992) {
-          mobileFunctions();
-        }
-      }
-
-      resize();
-    })();
   });
 })(jQuery);
